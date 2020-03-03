5caa3ad45985b2257ccc8d82477977032abc2861
João Silva <joaopssilva@brq.com>
2020-03-02 23:39:32 -0300

    Timer

--- poker-blinds-home-game/assets/js/index.js
+++ poker-blinds-home-game/assets/js/index.js
@@ -1,4 +1,6 @@
-﻿var torneio = {
+﻿var timerClock = 0;
+
+var torneio = {
     id: 1,
     nomeTorneio: "Home Game do João",
     dataHoraInicio: "",
@@ -8,8 +10,8 @@
             nivel: 1,
             small: 50,
             big: 100,
-            ante: 100,
-            duracao: 15 * 60,
+            ante: null,
+            duracao: 1 * 60,
             break: false
         },
         {
@@ -17,7 +19,119 @@
             small: 100,
             big: 200,
             ante: null,
-            duracao: 15 * 60,
+            duracao: 1 * 60,
+            break: false
+        },
+        {
+            nivel: 3,
+            small: 150,
+            big: 300,
+            ante: null,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 4,
+            small: 200,
+            big: 400,
+            ante: null,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 5,
+            small: 250,
+            big: 500,
+            ante: null,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 6,
+            small: 300,
+            big: 600,
+            ante: null,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: null,
+            small: null,
+            big: null,
+            ante: null,
+            duracao: 20 * 60,
+            break: true
+        },
+        {
+            nivel: 7,
+            small: 400,
+            big: 800,
+            ante: 800,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 8,
+            small: 500,
+            big: 1000,
+            ante: 1000,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 9,
+            small: 600,
+            big: 1200,
+            ante: 1200,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 10,
+            small: 800,
+            big: 1600,
+            ante: 1600,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 11,
+            small: 1000,
+            big: 2000,
+            ante: 2000,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 12,
+            small: 1500,
+            big: 3000,
+            ante: 3000,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 13,
+            small: 2000,
+            big: 4000,
+            ante: 4000,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 14,
+            small: 2500,
+            big: 5000,
+            ante: 5000,
+            duracao: 20 * 60,
+            break: false
+        },
+        {
+            nivel: 15,
+            small: 3000,
+            big: 6000,
+            ante: 6000,
+            duracao: 20 * 60,
             break: false
         }
     ],
@@ -212,28 +326,59 @@ jQuery(document).ready(function() {
     montarLeft();
     montarCenter();
     montarRight();
-
-
 });
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function() {
+        this.sound.play();
+    }
+    this.stop = function() {
+        this.sound.pause();
+    }
+}
 
 function montarLeft() {
     jQuery("#pb-info-title").text(torneio.nomeTorneio);
-    jQuery("#pb-info-level").text("Nível " + torneio.nivel);
     jQuery("#pb-settings").text("Configurações");
 }
 
 function montarCenter() {
-    var torneioNivel = torneio.nivel;
-    var blinds = obterPorValor(torneio.blinds, "nivel", torneioNivel);
-    montarBlinds(blinds);
-    montarTimer(blinds);
+    mudarNivel();
 
-    jQuery(".pb-button-play").on("click", function() {
-        var display = document.querySelector('#pb-timer');
-        startTimer(blinds.duracao, display);
+    jQuery(document).on("click", ".pb-button-play", function() {
+        startTimer();
+    });
+
+    jQuery(document).on("click", ".pb-button-stop", function() {
+        resetTimer();
     });
 
     jQuery("#pb-settings").text("Configurações");
+}
+
+function mudarNivel() {
+    var nivelRecuperado = localStorage.getItem("nivel");
+    var timerRecuperado = localStorage.getItem("timer");
+
+    if (nivelRecuperado == null) {
+        jQuery("#pb-info-level").text("Nível " + torneio.nivel);
+        var blinds = obterPorValor(torneio.blinds, "nivel", torneio.nivel);
+        montarBlinds(blinds);
+        montarTimer(blinds.duracao);
+    } else {
+        torneio.nivel = parseInt(nivelRecuperado);
+        jQuery("#pb-info-level").text("Nível " + torneio.nivel);
+        var blinds = obterPorValor(torneio.blinds, "nivel", torneio.nivel);
+        montarBlinds(blinds);
+        montarTimer(parseInt(timerRecuperado));
+        startTimer();
+    }
 }
 
 function montarRight() {
@@ -274,8 +419,15 @@ function montarBlinds(blinds) {
     jQuery("#pb-current-ante").text(blinds.ante);
 }
 
-function montarTimer(blinds) {
-    jQuery("#pb-timer").text(millisToMinutesAndSeconds(blinds.duracao * 1000));
+function montarTimer(timer) {
+
+    var minutes = parseInt(timer / 60, 10);
+    var seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    jQuery("#pb-timer").text(minutes + ":" + seconds);
 }
 
 // UTIL
@@ -287,35 +439,102 @@ function obterPorValor(array, propriedade, valor) {
 function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
-function startTimer(duration, display) {
+function startTimer() {
+    var display = document.querySelector('#pb-timer');
+    var blinds = obterPorValor(torneio.blinds, "nivel", torneio.nivel);
+    var duration = blinds.duracao;
+    var timerRecuperado = localStorage.getItem("timer");
+
+    if (timerRecuperado != null) {
+        duration = parseInt(timerRecuperado);
+    }
+
+    var turns = [
+        (duration / 3 * 2),
+        (duration / 3 * 1),
+        (duration / 3 * 0)
+    ];
+
+    var turn = 0;
+    var colorIncrease = 255 / (duration / 3);
+
     var timer = duration,
         minutes, seconds;
 
-    var red = 16;
-    var green = 139;
-    var blue = 63;
 
-    setInterval(function() {
+    var red = 0;
+    var green = 100;
+    var blue = 0;
+
+    timerClock = setInterval(function() {
+
+        localStorage.setItem('timer', timer);
+        localStorage.setItem('nivel', torneio.nivel);
+
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        //display.textContent = minutes + ":" + seconds;
+        jQuery("#pb-timer").text(minutes + ":" + seconds);
 
-        red++;
-        green--;
-
-        if (--timer < 0) {
-
-            timer = duration;
+        if (turn == 0) {
+            green = green + colorIncrease;
+        } else if (turn == 1) {
+            red = red + colorIncrease;
+            green = green + colorIncrease;
+        } else if (turn == 2) {
+            red = red + colorIncrease;
         }
 
-        jQuery(".circle").css({ "border": "30px solid rgb(" + red + "," + green + "," + blue + ")" });
+        if (timer == turns[turn]) {
+
+
+            turn = turn + 1;
+
+            if (turn == 1) {
+                green = 100;
+                blue = 0;
+                red = 100;
+            } else {
+                green = 0;
+                blue = 0;
+                red = 100;
+            }
+        }
+
+        if (--timer < 0) {
+            var mySound = new sound("./assets/sons/sino.mp3");
+            mySound.play();
+            localStorage.clear();
+            timer = duration;
+            stopTimer();
+            torneio.nivel = torneio.nivel + 1;
+            mudarNivel();
+            startTimer();
+        } else {
+            jQuery(".circle").css({ "border": "2.2em solid rgb(" + red + "," + green + "," + blue + ")" });
+        }
 
     }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerClock);
+}
+
+function resetTimer() {
+    torneio.nivel = 1;
+    localStorage.clear();
+    stopTimer();
+    montarCenter();
+}
+
+function changeBlinds() {
+    torneio.nivel
 }
